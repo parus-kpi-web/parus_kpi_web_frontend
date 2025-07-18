daea696c5332618290adbbcf26a1e6283d6ac640
aleksandr <volokhov2004@icloud.com>
2025-07-18 23:06:00 +0300

    split wip

--- src/components/organisms/SplitPane/SplitPaneLayout.tsx
+++ src/components/organisms/SplitPane/SplitPaneLayout.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
-import { ReflexContainer, ReflexSplitter, ReflexElement } from 'react-reflex';
+import {ReflexContainer, ReflexSplitter, ReflexElement} from 'react-reflex';
 import 'react-reflex/styles.css';
 
 export interface SplitPaneLayoutProps {
-    left: React.ReactNode;
-    right: React.ReactNode;
+    panes: React.ReactNode[];
     orientation?: 'vertical' | 'horizontal';
 }
 
 const SplitPaneLayout: React.FC<SplitPaneLayoutProps> = ({
-                                                             left,
-                                                             right,
+                                                             panes,
                                                              orientation = 'horizontal',
-                                                         }) => (
-    <ReflexContainer orientation={orientation}>
-        <ReflexElement>{left}</ReflexElement>
-        <ReflexSplitter />
-        <ReflexElement>{right}</ReflexElement>
-    </ReflexContainer>
-);
+                                                         }) => {
+    const children: React.ReactNode[] = [];
+    panes.forEach((pane, idx) => {
+        children.push(
+            <ReflexElement key={`pane-${idx}`} flex={1} propagateDimensions>
+                {pane}
+            </ReflexElement>
+        );
+        if (idx < panes.length - 1) {
+            children.push(
+                <ReflexSplitter key={`splitter-${idx}`} propagate/>
+            );
+        }
+    });
+    return (
+        <ReflexContainer orientation={orientation} style={{height: '100%', width: '100%'}}>
+            {children}
+        </ReflexContainer>
+    );
+};
 
 export default SplitPaneLayout;
--- src/components/templates/DashboardTemplate.tsx
+++ src/components/templates/DashboardTemplate.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Layout, SplitPaneLayout, Table } from '../../components';
+import {Layout, SplitPaneLayout, Table} from '../../components';
 
 const DashboardTemplate: React.FC = () => {
     const [collapsed, setCollapsed] = React.useState(false);
 
+    // пять таблиц-заглушек
+    const tables = Array.from({length: 5}, (_, i) => (
+        <Table key={i} data={[]} columns={[]}/>
+    ));
+
     return (
-        <Layout collapsed={collapsed} toggle={() => setCollapsed((c) => !c)}>
-            <SplitPaneLayout
-                orientation="horizontal"
-                left={<Table data={[]} columns={[]}/>}
-                right={<Table data={[]} columns={[]}/>}
-            />
+        <Layout collapsed={collapsed} toggle={() => setCollapsed(c => !c)}>
+            <SplitPaneLayout orientation="horizontal" panes={tables}/>
         </Layout>
     );
 };
